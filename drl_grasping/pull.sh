9c216cd806bf0ea22a8e148e5c361d6985fdec86
Enric Cervera <ecervera@uji.es>
2025-07-23 16:03:37 +0200

    Add repo

---
+++ drl_grasping/pull.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+docker pull ghcr.io/robinlabuji/ros2rep/drl_grasping:galactic
+
---
+++ drl_grasping/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+rocker --nvidia --x11 ghcr.io/robinlabuji/ros2rep/drl_grasping:galactic
+
