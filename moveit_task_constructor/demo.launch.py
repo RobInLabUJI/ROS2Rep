5aa24a939608941762f7034e88be2b3991a04ec0
Enric Cervera <ecervera@uji.es>
2025-07-23 17:17:06 +0200

    Fixes

--- moveit_task_constructor/demo.launch.py
+++ moveit_task_constructor/demo.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
     moveit2_tutorials_dir = get_package_share_directory('moveit2_tutorials')
     
     mtc_demo_launch = os.path.join(moveit2_tutorials_dir, 'launch', 'mtc_demo.launch.py')
-    mtc_demo_minimal_launch = os.path.join(moveit2_tutorials_dir, 'launch', 'mtc_demo_minimal.launch.py')
+    mtc_minimal_launch = os.path.join(moveit2_tutorials_dir, 'launch', 'mtc_demo_minimal.launch.py')
 
     mtc_demo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(mtc_demo_launch)
