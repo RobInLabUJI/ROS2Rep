d29f9b1e9db9c88a33c1b960bb1ad74ab8e04ddb
Enric Cervera <ecervera@uji.es>
2025-07-23 17:10:12 +0200

    Add launch file

---
+++ moveit_task_constructor/demo.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    moveit2_tutorials_dir = get_package_share_directory('moveit2_tutorials')
+    
+    mtc_demo_launch = os.path.join(moveit2_tutorials_dir, 'launch', 'mtc_demo.launch.py')
+    mtc_demo_minimal_launch = os.path.join(moveit2_tutorials_dir, 'launch', 'mtc_demo_minimal.launch.py')
+
+    mtc_demo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(mtc_demo_launch)
+    )
+    
+    mtc_demo_minimal = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(mtc_demo_minimal_launch)
+    )
+    
+    return LaunchDescription([
+        mtc_demo,
+        mtc_demo_minimal
+    ])
