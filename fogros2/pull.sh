289425f7bda89cf0eb7f9918b6461293c356d4f3
Enric Cervera <ecervera@uji.es>
2025-07-24 10:33:23 +0200

    Add repo

---
+++ fogros2/pull.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+docker pull ghcr.io/robinlabuji/ros2rep/fogros2:humble
+
---
+++ fogros2/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+rocker --devices /dev/dri --x11 ghcr.io/robinlabuji/ros2rep/fogros2:humble bash
+
